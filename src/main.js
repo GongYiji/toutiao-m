8080c92e30c90f732efe02ead357571eb30d5443
gongyijie666 <10340347+gongyijie666@user.noreply.gitee.com>
2022-05-31 23:37:03 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+// 引入vant所有组件
+import Vant from 'vant';
+//加载 vant 全局样式
+import 'vant/lib/index.css';
 
-//测试
-import './styles/index.less'
+//注册使用 vant组件库
+Vue.use(Vant);
+
+
+
+
+
 
 Vue.config.productionTip = false
 
